c278e94805b0d76a0e1710115b877030e5295bf0
memosk8 <memolopez9363@gmail.com>
2023-03-30 15:36:27 -0600

    added methods: prepend, pop_first, get, set_value, insert, remove

--- EXERCISE-LL-Constructor.py
+++ EXERCISE-LL-Constructor.py
@@ -1,11 +1,11 @@
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
 
 
 class LinkedList:
-    def __init__(self,value):
+    def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
@@ -17,9 +17,9 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
 
-    def append(self,value):
+    def append(self, value):
         new_node = Node(value)
-        if(self.head is None):
+        if (self.head is None):
             self.head = new_node
             self.tail = new_node
         else:
@@ -29,11 +29,11 @@ class LinkedList:
         return True
 
     def pop(self):
-        if(self.length == 0):
+        if (self.length == 0):
             return None
         pre = self.head
         temp = self.head
-        while(temp.next):
+        while (temp.next):
             pre = temp
             temp = temp.next
         self.tail = pre
@@ -44,16 +44,76 @@ class LinkedList:
             self.tail = None
         return temp
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
 
-my_linked_list = LinkedList(10)
+    def pop_first(self):
+        if self.length == 0 :
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length =- 1
+        if self.length == 0:
+            self.tail = None
+        return temp
 
-my_linked_list.append(2)
-my_linked_list.append(23)
-my_linked_list.append(78)
-my_linked_list.print_list()
-print("-")
-my_linked_list.pop()
-my_linked_list.pop()
-my_linked_list.pop()
-print(my_linked_list.pop().value)
+    def get(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self,index,value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+        
+
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length += 1
+        return temp
+
+
+my_linked_list = LinkedList(0)
+
+my_linked_list.append(1)
+my_linked_list.insert(2,23)
+
 my_linked_list.print_list()
